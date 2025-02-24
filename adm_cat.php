2604916e1d2e84794914b705c3a08f62a7428bec
Samuel S. <samuelsiqueiralourenco@gmail.com>
2025-02-24 15:03:39 -0300

    24/02/25

---
+++ adm_cat.php
@@ -0,0 +1,8 @@
+<?php
+    $nome=$_POST['txt_nome'];
+    $descricao=$_POST['txt_descricao'];
+    echo("O nome da categoria é" .$nome);
+echo("<BR>");
+    echo("A descrição da categoria é" .$descricao);
+
+?>
